7c2a3f44fc158588861c2ef2539f67d8b5cb6296
cr4bbz <voigtjulianleon@gmail.com>
2026-06-29 14:13:14 +0200

    Genesis: add Rocq deontic example

---
+++ provers/rocq/theories/Deontic.v
@@ -0,0 +1,26 @@
+From Coq Require Import Init.Logic.
+
+Module Deontic.
+
+Parameter World : Type.
+Parameter Obl : World -> Prop.
+Parameter Perm : World -> Prop.
+
+Axiom obligation_implies_permission :
+  forall w : World, Obl w -> Perm w.
+
+Theorem permission_from_obligation :
+  forall w : World, Obl w -> Perm w.
+Proof.
+  intros w h.
+  apply obligation_implies_permission.
+  exact h.
+Qed.
+
+Theorem obligation_entails_permission :
+  forall w : World, Obl w -> Perm w.
+Proof.
+  exact obligation_implies_permission.
+Qed.
+
+End Deontic.
